ec6b8e8812de2901d265da91c8d3e87b8654ffd0
marketing2cimc-rgb <marketing2.cimc@gmail.com>
2025-09-02 14:43:38 +0700

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,4 @@
 import "./../styles/globals.css";
-import { Sidebar } from "../components/Sidebar";
-import { Navbar } from "../components/Navbar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
