26422009169104b902ca0144eebabf5a845ee1b3
marketing2cimc-rgb <marketing2.cimc@gmail.com>
2025-09-02 14:47:07 +0700

    Update page.tsx

--- frontend/src/app/customers/page.tsx
+++ frontend/src/app/customers/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useState } from "react";
+import { Navbar } from "@/components/Navbar";
+import { Sidebar } from "@/components/Sidebar";
 import { Card } from "@/components/Card";
 
 interface Customer {
