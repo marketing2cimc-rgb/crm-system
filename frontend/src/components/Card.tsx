2c1664e66ff1c0a9f59015ed685ea243ab9a2a7e
marketing2cimc-rgb <marketing2.cimc@gmail.com>
2025-09-02 14:24:12 +0700

    Update Card.tsx

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -4,14 +4,16 @@ interface CardProps {
   title: string;
   value: string | number;
   description?: string;
+  children?: React.ReactNode; // ✅ 添加 children
 }
 
-export const Card = ({ title, value, description }: CardProps) => {
+export const Card = ({ title, value, description, children}: CardProps) => {
   return (
     <div className="bg-white shadow-md rounded-xl p-4 border border-gray-200 hover:shadow-lg transition">
       <h2 className="text-sm text-gray-500">{title}</h2>
       <p className="text-2xl font-bold text-gray-800">{value}</p>
       {description && <p className="text-xs text-gray-400">{description}</p>}
+	  <div>{children}</div>
     </div>
   );
 };
