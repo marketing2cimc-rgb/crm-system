9a825334cea09c08cbbaef361939b8872788e1d9
marketing2cimc-rgb <marketing2.cimc@gmail.com>
2025-09-02 11:35:54 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const cors = require("cors");
+
 app.use(cors({
-  origin: ["https://crm-system-ekzz.onrender.com"],
+  origin: [
+    "https://crm-system-ekzz.onrender.com", // 你的前端地址（Vercel）
+    "http://localhost:3000" // 本地调试时用
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
