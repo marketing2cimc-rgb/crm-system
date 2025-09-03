334b8f333bbf6f6a065a1dfb36069b88d638af3d
marketing2cimc-rgb <marketing2.cimc@gmail.com>
2025-09-03 10:54:34 +0700

    Update auth.controller.ts

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -22,7 +22,7 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
 
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
